94a85889ca4c6c3d16fb08cd0d5c4a7db3b28be5
HebaSalem <puupiiiii@gmail.com>
2022-08-18 18:01:59 +0200

    cretae and style header section

--- js/script.js
+++ js/script.js
@@ -11,3 +11,22 @@ window.addEventListener('scroll' , ()=> {
     nav.classList.remove('scroll')
   }
 })
+
+//validate form input fields
+const errorMessage = document.querySelectorAll('form .form-div .error-message')
+const errorIcons = document.querySelectorAll('form .form-div .error-icon')
+const inputs = document.querySelectorAll('form input')
+
+for(let i=0 ; i<inputs.length ; i++) {
+  inputs[i].addEventListener('blur' , ()=> {
+
+    if(inputs[i].value.length === 0) {
+      errorIcons[i].style.display='block'
+      errorMessage[i].style.display='block'
+    } else {
+      errorIcons[i].style.display='none'
+      errorMessage[i].style.display='none'
+    }
+  })
+
+}
